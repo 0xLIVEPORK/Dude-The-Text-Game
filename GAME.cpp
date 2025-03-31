80504522d5f60943d40037ea6845faf7dc2cda4c
EVILPORK <0xlivepork@gmail.com>
2025-03-31 23:42:27 +0800

    Update GAME.cpp

--- GAME.cpp
+++ GAME.cpp
@@ -6,11 +6,18 @@
 int main () {
 
     int test;
+    std::cout << 
+    "Menu: \n"
+    "[0] PLAY" << "\n" <<
+    "[1] SAVE FILES" << "\n" <<
+    "[2] CREDITS" << "\n" <<
+    "[3] EXIT" << "\n";
+
 
     do
     {
-    std::cout << "Enter a Valid Integer not more than 10 or less than 0: ";
-        if(!(std::cin >> test) || test < 0 || test > 10)
+    std::cout << "Enter a Valid Integer not more than 3 or less than 0: ";
+        if(!(std::cin >> test) || test < 0 || test > 3)
         {
             std::cerr << "Invalid Input\nTry Again: ";
             std::cin.clear();
