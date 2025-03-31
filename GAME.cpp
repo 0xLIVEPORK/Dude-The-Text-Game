7da7366fb4e57de00ebb64eb2bab0e8a70bc48df
EVILPORK <0xlivepork@gmail.com>
2025-03-31 23:14:30 +0800

    Update GAME.cpp

--- GAME.cpp
+++ GAME.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <random>
 #include <string>
-#include <ctime>
+#include <limits>
 
-std::string menu_loop() {};
-int main(){
+int main () {
 
-  return 0;
+    int test;
+
+    do
+    {
+    std::cout << "Enter a Valid Integer not more than 10 or less than 0: ";
+        if(!(std::cin >> test) || test < 0 || test > 10)
+        {
+            std::cerr << "Invalid Input\nTry Again: ";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+    break;
+    }
+    while (true);
 }
