6b66b8d2d3af35ff679ea1f3321c076b1a32cba9
EVILPORK <0xlivepork@gmail.com>
2025-04-14 23:50:54 +0800

    Update GAME.cpp

--- GAME.cpp
+++ GAME.cpp
@@ -4,41 +4,6 @@
 #include <limits>
 
 
-int MenuLoop(int &test);
-
-struct character{
-    std::string name = "Kei";
-    unsigned short level = 2;
-    unsigned short health = 5;
-    unsigned short strength = rand(); 
-    std::string weapon = "Bread";
-
-    void attack(std::string& name, unsigned short& level,  unsigned short& health, unsigned short& strength, std::string& weapon) {
-        std::cout << name << " attacks with " << weapon << " for " << strength * 2 << " damage!\n";
-    }
-};
-
-void display(std::string& name, unsigned short& level,  unsigned short& health, unsigned short& strength, std::string& weapon) {
-    std::cout 
-    << "Character: " << name << "\n"
-    << "Level: " << level << "\n"
-    << "Health: " << health << "\n"
-    << "Strength " << strength << "\n"
-    << "Weapon: " << weapon << "\n";
-}
-
-int main() {
-    srand(time(0));
-
-    character Kei;
-
-    display(Kei.name, Kei.level, Kei.health, Kei.strength, Kei.weapon);
-
-    Kei.attack(Kei.name, Kei.level, Kei.health, Kei.strength, Kei.weapon);
-    
-    return 0;
-}
-
 
 int MenuLoop(int &test);
 
