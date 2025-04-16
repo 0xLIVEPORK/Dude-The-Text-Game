0a26fc81d848df55064d262f85c9eacb66c6e370
EVILPORK <0xlivepork@gmail.com>
2025-04-16 21:38:20 +0800

    Update GAME.cpp

--- GAME.cpp
+++ GAME.cpp
@@ -2,37 +2,30 @@
 #include <random>
 #include <string>
 #include <limits>
-
-
+#include <fstream>
 
 int MenuLoop(int &test);
 
-int main () {
-    int test = 0;
+struct character{
+    std::string name;
+    unsigned short level;
+    unsigned short health;
+    unsigned short strength;
+    std::string weapon;
 
-    MenuLoop(test);
-
-    switch (test) 
-    {
-        case 0:
-        std::cout << "WE ARE PLAYING!";
-        break;
-
-        case 1:
-        std::cout << "> FILE 1\n" << " FILE 2\n";
-        break;
-        
-        case 2:
-        std::cout << "\t\tCREDITS\n" << "\n0xLIVEPORK - PROGRAMMER\n";
-        break;
-        
-        case 3:
-        std::cout << "THANKS FOR PLAYING!";
-        break; 
-        
-        default:
-        break;
+    void attack(std::string& name, unsigned short& level,  unsigned short& health, unsigned short& strength, std::string& weapon) {
+        std::cout << name << " attacks with" << weapon << " for" << strength * 2 << " damage!\n";
     }
+};
+
+void display(std::string& name, unsigned short& level,  unsigned short& health, unsigned short& strength, std::string& weapon) {
+    
+}
+
+
+
+int main () {
+    i
 
 return 0;
 }
@@ -46,4 +39,21 @@ int MenuLoop(int &test) {
     "[2] CREDITS\n"
     "[3] EXIT\n";
 
+    std::cout << Menu;
 
+    do
+    {
+        if(!(std::cin >> test) || test < 0 || test > 3)
+        {
+            std::cout << Menu;
+            std::cout << "Invalid Input\nTry Again: ";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+    break;
+    }
+    while (true);
+
+    return test;
+}
