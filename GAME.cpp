af606fc9a6b357f09cf2b1a13cdf3045884d5cba
EVILPORK <0xlivepork@gmail.com>
2025-04-16 22:11:23 +0800

    Update GAME.cpp

--- GAME.cpp
+++ GAME.cpp
@@ -17,22 +17,3 @@ class Character{
 
 
 
-#include <iostream>
-#include <random>
-#include <string>
-#include <limits>
-#include <fstream>
-
-class Character{
-    Character(unsigned short x, short y, std::string z) {
-        unsigned short health = x;
-        short stamina = y;
-        std::string weapon = z;
-    }
-
-    void Inventory();
-};
-
-
-
-
