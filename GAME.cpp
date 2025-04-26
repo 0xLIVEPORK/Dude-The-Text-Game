98bc51f9f7c7c8411d92d3e7f6c7dc52d314d0ee
EVILPORK <0xlivepork@gmail.com>
2025-04-26 16:14:17 +0800

    Update GAME.cpp

--- GAME.cpp
+++ GAME.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <limits>
 
 class Weapon{
   private:
@@ -16,15 +17,55 @@ class Weapon{
   {
     std::cout << "Weapon Name: <" << name << ">, Damage: <" << damage << "> \n";
   }
+
+  void setname()
+  {
+  std::string new_name;
+    while (true)
+    {
+    std::cout << "\nEnter a New Name for your " << name << ": ";
+      if(!(getline(std::cin, new_name)))
+      {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        continue;
+      }
+    name.clear();
+    name = new_name;
+    break;
+    }
+  }
+
+  void setdmg()
+  {
+  int dmg_factor;
+    while (true)
+    {
+    std::cout << "\nEnter a New Name for your " << name << ": ";
+      if(!(getline(std::cin, new_name)))
+      {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        continue;
+      }
+    name.clear();
+    name = new_name;
+    break;
+    }
+  }
 };
 
 int main() {
 
   Weapon Morning_Star("Morning Star", 5);
   Morning_Star.describeWeapon();
+  Morning_Star.setname();
+  Morning_Star.describeWeapon();
 
   Weapon Halberd("Halberd", 20);
   Halberd.describeWeapon();
+
+
 
   return EXIT_SUCCESS;
 }
