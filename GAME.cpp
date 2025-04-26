dde62ef236e56096386dce61c7bccc10c3a7988e
EVILPORK <0xlivepork@gmail.com>
2025-04-26 22:17:08 +0800

    Update GAME.cpp

--- GAME.cpp
+++ GAME.cpp
@@ -1,78 +1,72 @@
-/*
-✍️ YOUR TASK
-
-👉 Create a new class called Armor.
-It should have:
-
-    Two private variables: std::string material; and int defense;
-
-    A constructor that sets both.
-
-    A getter for material.
-
-    A getter for defense.
-
-    A setter for material.
-
-    A setter for defense.
-
-    A describeArmor() method that prints the material and defense.
-*/
-
 #include <iostream>
-#include <limits>
 #include <cstdlib>
+#include <limits>
 
-class Armor{
 
-    private:
+class Inventory{
+  protected:
 
-    std::string material;
-    int defense;
-
-    public:
-    // Constructadinos
-    Armor(std::string m, int d) : material(m), defense(d)  {}
-    Armor(std::string m) : material(m), defense(5) {}
-
-    // Getterinos
-    void getMaterial() {
-        std::cout << material;
-    }
-
-    void getDefense() {
-        std::cout << defense;
-    }
-
-    // Setterinos
-
-    void setMaterial() {
-
-        std::cout << "Enter the New Material For Armor: \n";
-        getline(std::cin, material);
-    }
-
-    void setDefense() {
-        
-        int new_defense;
-        
-        while (true)
-        {
-            std::cout << "Set the New Value for Armor Defense: \n";
-
-            if(!(std::cin >> new_defense) || new_defense > 1000 || new_defense < 0)
-            {
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                continue;
-            }
-        defense = new_defense;
-        break;
-        }
-        
-    }
-
-    void describeArmor() {
-        
-    }
+  std::string inventory[10];
 };
+
+
+class Weapon : Inventory{
+  private:
+
+  std::string name;
+  int damage;
+
+
+  public:
+  // Have to put comments cause my monitor is shit and I need comments to not get confused by the dark balance of my monitor
+  // CONSTRUCTORS
+  Weapon(std::string n, int d) : name(n), damage(d) {}
+
+  Weapon(std::string n) : name(n), damage(0) {}
+  // CONSTRUCTORS
+
+  void describeWeapon()
+  {
+    std::cout << "Weapon Name: <" << name << ">, Damage: <" << damage << "> \n";
+  }
+
+  // SETTERS
+  void setname()
+  {
+  std::string new_name;
+    while (true)
+    {
+    std::cout << "\nEnter a New Name for your " << name << ": ";
+      if(!(getline(std::cin, new_name)))
+      {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        continue; 
+      }
+
+    name = new_name;
+    break;
+    }
+  }
+
+  void setdmg()
+  {
+  inventory; //imma leave this cause Idk jack shit about inheritance
+  }
+};
+
+int main() {
+
+  Weapon Morning_Star("Morning Star", 5);
+  Morning_Star.describeWeapon();
+  Morning_Star.setname();
+  Morning_Star.describeWeapon();
+
+  Weapon Halberd("Halberd");
+  Halberd.describeWeapon();
+
+
+
+  return EXIT_SUCCESS;
+
+}
